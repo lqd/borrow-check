244d6cc4a4bfa9628925b5589afe183f766a409e
Rémy Rakic <remy.rakic@gmail.com>
2019-06-07 11:43:04 +0200

    Add an example where cycle unification should work

--- inputs/polonius-imprecision/polonius-imprecision.rs
+++ inputs/polonius-imprecision/polonius-imprecision.rs
@@ -11,14 +11,28 @@ fn unnecessary_error() {
     }
 
     if false {
-        x.0 = &z; // creates {L0} in 'x constraint
-        // at this point, we have `'x: 'y` and `{L0} in `'x`, so we also have `{L0} in 'y`
+        x.0 = &z; // creates `{L0} in 'x` constraint
+        // at this point, we have `'x: 'y` and `{L0} in 'x`, so we also have `{L0} in 'y`
         drop(x.0);
     }
 
     z += 1; // polonius flags an (unnecessary) error
 
     drop(y.0);
+}
+
+fn cycle_unification() {
+    let mut x = 0;
+    let mut v: Vec<&u32> = vec![]; // `'v`
+
+    let p: &mut Vec<&u32> = &mut v; // `'p`, which could be unified with `'v`
+    let q = &x; // creates `{L0}`
+
+    p.push(q); // adds `{L0}` to `'p`, needs to *indirectly* add to `'v`.
+    // If unified, this would add `{L0}` to `'v`
+    
+    x += 1; // error. Unified: an error because `{L0} in 'v` and `v` is live
+    drop(v);
 }
 
 fn main() {}
