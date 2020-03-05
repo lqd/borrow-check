2728e48259a558c7a584830e6f6d2b09c0befc51
Rémy Rakic <remy.rakic@gmail.com>
2020-03-05 01:33:50 +0100

    add a couple more subset-relations tests, including one showing the known_subset relation emitted by rustc is not closed over

--- inputs/subset-relations/subset-relations.rs
+++ inputs/subset-relations/subset-relations.rs
@@ -1,14 +1,29 @@
 #![crate_type = "lib"]
 #![feature(nll)]
+#![allow(unused_assignments)]
+#![allow(unused_variables)]
 
-fn missing_subset<'a, 'b>(x: &'a u32, y: &'b u32) -> &'a u32 {
-    y //~ ERROR
+fn missing_subset<'a, 'b>(a: &'a u32, b: &'b u32) -> &'a u32 {
+    b //~ ERROR lifetime may not live long enough
+}
+
+fn missing_transitive_subsets<'a, 'b, 'c>(a: &'a u32, mut b: &'b u32, mut c: &'c u32) {
+    b = a; //~ lifetime may not live long enough
+    c = b; //~ lifetime may not live long enough
+    //^ lifetime may not live long enough
 }
 
 fn implied_bounds_subset<'a, 'b>(x: &'a &'b mut u32) -> &'a u32 {
     x
 }
 
-fn valid_subset<'a, 'b: 'a>(x: &'a u32, y: &'b u32) -> &'a u32 {
-    y
+fn valid_subset<'a, 'b: 'a>(a: &'a u32, b: &'b u32) -> &'a u32 {
+    b
+}
+
+// As `'a: 'c` holds transitively, it will not be emitted in the `known_subset` relation,
+// Polonius needs to compute its transitive closure to not miss the relationship is present.
+fn valid_transitive_subsets<'a: 'b, 'b: 'c, 'c>(a: &'a u32, mut b: &'b u32, mut c: &'c u32) {
+    b = a;
+    c = b;
 }
